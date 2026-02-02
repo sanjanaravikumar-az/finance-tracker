b98a959ced2380f0513a75757f0f841611644f1b
sanjanaravikumar-az <sanjana.ravikumar.az@gmail.com>
2026-02-02 14:53:22 -0500

    Add external AWS SDK bundling configuration

--- amplify/function/financetrackere30b1453/resource.ts
+++ amplify/function/financetrackere30b1453/resource.ts
@@ -8,5 +8,12 @@ export const financetrackere30b1453 = defineFunction({
     timeoutSeconds: 25,
     memoryMB: 128,
     environment: { BUDGET_ALERT_TOPIC_ARN: "arn:aws:sns:us-east-1:079385506759:finance-budget-alerts-main", MONTHLY_REPORT_TOPIC_ARN: "arn:aws:sns:us-east-1:079385506759:finance-monthly-reports-main", ENV: `${branchName}`, REGION: "us-east-1" },
-    runtime: 22
+    runtime: 22,
+    bundling: {
+        format: "esm",
+        minify: false,
+        esbuildArgs: {
+            "--external:@aws-sdk/*": ""
+        }
+    }
 });
