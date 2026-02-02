7b7862b1b7779ab53bbcdb9c07af88b6b5de893e
sanjanaravikumar-az <sanjana.ravikumar.az@gmail.com>
2026-02-02 15:09:19 -0500

    Remove function import from auth resource to break circular dependency

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,5 +1,4 @@
 import { defineAuth } from "@aws-amplify/backend";
-import { financetrackere30b1453 } from "../function/financetrackere30b1453/resource";
 
 export const auth = defineAuth({
     loginWith: {
@@ -16,22 +15,5 @@ export const auth = defineAuth({
     },
     multifactor: {
         mode: "OFF"
-    },
-    access: (allow: any) => [
-        allow.resource(financetrackere30b1453).to(["manageGroupMembership"]),
-        allow.resource(financetrackere30b1453).to(["managePasswordRecovery"]),
-        allow.resource(financetrackere30b1453).to(["createUser"]),
-        allow.resource(financetrackere30b1453).to(["setUserSettings"]),
-        allow.resource(financetrackere30b1453).to(["manageUsers"]),
-        allow.resource(financetrackere30b1453).to(["disableUser"]),
-        allow.resource(financetrackere30b1453).to(["setUserMfaPreference"]),
-        allow.resource(financetrackere30b1453).to(["updateUserAttributes"]),
-        allow.resource(financetrackere30b1453).to(["forgetDevice"]),
-        allow.resource(financetrackere30b1453).to(["enableUser"]),
-        allow.resource(financetrackere30b1453).to(["updateDeviceStatus"]),
-        allow.resource(financetrackere30b1453).to(["getDevice"]),
-        allow.resource(financetrackere30b1453).to(["getUser"]),
-        allow.resource(financetrackere30b1453).to(["deleteUserAttributes"]),
-        allow.resource(financetrackere30b1453).to(["deleteUser"])
-    ],
+    }
 });
